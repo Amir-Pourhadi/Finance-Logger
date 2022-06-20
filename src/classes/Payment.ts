03b5a8c89c0655280c1640f4638b613caea93140
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-20 21:31:04 +0430

    Payment class

---
+++ src/classes/Payment.ts
@@ -0,0 +1,9 @@
+import { HasFormatter } from "../interfaces/HasFormatter";
+
+export class Payment implements HasFormatter {
+  constructor(readonly recipient: string, private details: string, public amount: number) {}
+
+  format() {
+    return `${this.recipient} is owed £${this.amount} for ${this.details}`;
+  }
+}
