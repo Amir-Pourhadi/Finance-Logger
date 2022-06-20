eb7312dae40203b67c3cbf4ee0698f4431422343
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-20 21:30:31 +0430

    Invoice class

---
+++ src/classes/Invoice.ts
@@ -0,0 +1,9 @@
+import { HasFormatter } from "../interfaces/HasFormatter";
+
+export class Invoice implements HasFormatter {
+  constructor(readonly client: string, private details: string, public amount: number) {}
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
