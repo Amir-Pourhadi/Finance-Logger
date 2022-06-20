c3deb37fe154aef2477377b487514e6299367aee
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-20 17:26:23 +0430

    Dom Manipulation

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
 "use strict";
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+const form = document.querySelector("form.new-item-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
